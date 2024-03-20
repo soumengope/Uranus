7e3e205192ef14ac8d6dcc74ad8aed68932bd3f5
Soumen Gope <124480121+soumengope@users.noreply.github.com>
2024-03-20 20:04:21 +0530

    Update main.js
    change pre-launch text color

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ function scrollToTop(){
             clearInterval(changePos)
             rocket.style.bottom=`${0}rem`;
             rocket_txt.innerText = 'Pre-Launch';
-            rocket_txt.style.color = 'rgb(187, 48, 23)';
+            rocket_txt.style.color = '#26c7c4';
         }
     },10)
 }
